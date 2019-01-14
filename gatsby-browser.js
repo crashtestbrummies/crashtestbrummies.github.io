d98a0f96417930aacd8f00395f553cb18ab3fb06
Ad Taylor <425238+adtaylor@users.noreply.github.com>
2019-01-14 15:08:55 +0000

    Adds anchorate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+const anchorate = require('anchorate').anchorate
+
+exports.onRouteUpdate = () => {
+  anchorate({
+    scroller: function (element) {
+      if (!element) return false
+      element.scrollIntoView({ behavior: 'smooth', block: 'center' })
+      return true
+    }
+  })
+}
